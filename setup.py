32d7d4b5892261ac614efa8efbaefcc3873590f5
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2018-10-25 14:53:44 -0600

    Long description in setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,17 @@ if sys.version_info[0] == 3:
 elif sys.version_info[0] == 2:
     requires = ['dnspython']
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='clientsubnetoption',
       version='2.1.1',
       maintainer='Brian Hartvigsen',
       maintainer_email='bhartvigsen@opendns.com',
       description='EDNS Client Subnet option support for dnspython',
       url='https://github.com/opendns/dnspython-clientsubnetoption',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       license='BSD',
       py_modules=['clientsubnetoption'],
       install_requires=requires,
